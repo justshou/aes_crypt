6a581e888213fda586c26f6a1a2b1c690351493f
Tony Shou <a.mashou04@gmail.com>
2025-12-02 03:17:18 -0500

    Fixed pointers for encrypt/decrypt buffers Added to copypaste.txt

--- aes-256-cbc.cpp
+++ aes-256-cbc.cpp
@@ -6,20 +6,60 @@
 int encrypt(EVP_CIPHER_CTX* ctx, FILE* inFile, FILE* outFile, const unsigned char* key, const unsigned char* iv) {
     const EVP_CIPHER* cipher = EVP_aes_256_cbc();
 
-    char inputBuffer[1024];
-    char outputBuffer[1024 + EVP_MAX_BLOCK_LENGTH];
-
+    unsigned char inputBuffer[1024];
+    unsigned char outputBuffer[1024 + EVP_MAX_BLOCK_LENGTH];
     size_t bytesRead;
+    int final;
+
+    if (EVP_EncryptInit_ex(ctx, cipher, nullptr, key, iv) != 1) {
+        std::cout << "EncryptionInit failed (line 12)" << std::endl;
+        return -1;
+    }
+
     while ((bytesRead = std::fread(inputBuffer, 1, sizeof(inputBuffer), inFile)) > 0) {
         int outLen;
-        if (1 != EVP_EncryptUpdate(ctx, reinterpret_cast<unsigned char*>(outputBuffer), &outLen,
-                                   reinterpret_cast<unsigned char*>(inputBuffer), bytesRead)) {
-            std::cout << "Encryption failed (line 17)" << std::endl;
+        if (1 != EVP_EncryptUpdate(ctx, outputBuffer, &outLen, inputBuffer, bytesRead)) {
+            std::cout << "EncryptionUpdate failed (line 17)" << std::endl;
             return -1;
         }
         std::fwrite(outputBuffer, 1, outLen, outFile);
     }
 
+    if (EVP_EncryptFinal_ex(ctx, outputBuffer, &final) != 1) {
+        std::cout << "EncryptionFinal failed (line 24)" << std::endl;
+        return -1;
+    }
+    std::fwrite(outputBuffer, 1, final, outFile);
+    return 0;
+}
+
+int decrypt(EVP_CIPHER_CTX* ctx, FILE* inFile, FILE* outFile, const unsigned char* key, const unsigned char* iv) {
+    const EVP_CIPHER* cipher = EVP_aes_256_cbc();
+
+    unsigned char inputBuffer[1024];
+    unsigned char outputBuffer[1024 + EVP_MAX_BLOCK_LENGTH];
+    size_t bytesRead;
+    int final;
+
+    if (EVP_DecryptInit_ex(ctx, cipher, nullptr, key, iv) != 1) {
+        std::cout << "DecryptionInit failed" << std::endl;
+        return -1;
+    }
+
+    while ((bytesRead = std::fread(inputBuffer, 1, sizeof(inputBuffer), inFile)) > 0) {
+        int outLen;
+        if (1 != EVP_DecryptUpdate(ctx, outputBuffer, &outLen, inputBuffer, bytesRead)) {
+            std::cout << "DecryptionUpdate failed" << std::endl;
+            return -1;
+        }
+        std::fwrite(outputBuffer, 1, outLen, outFile);
+    }
+
+    if (EVP_DecryptFinal_ex(ctx, outputBuffer, &final) != 1) {
+        std::cout << "DecryptionFinal failed" << std::endl;
+        return -1;
+    }
+    std::fwrite(outputBuffer, 1, final, outFile);
     return 0;
 }
 
@@ -52,8 +92,9 @@ int main(int argc, char* argv[]) {
     FILE *outFile = fopen(argv[2], "wb");
 
     //convert with EVP_bytestokey
-    EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha256(), salted ? salt : nullptr, reinterpret_cast<const unsigned char*>(argv[3]), strlen(argv[3]), 1, key, iv);
-    const char *key = argv[3];
+    EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha256(), nullptr, reinterpret_cast<const unsigned char*>(argv[3]), strlen(argv[3]), 1, key, iv);
+    // std::cout << "key test: " << key << std::endl;
+    // std::cout << "iv test: " << iv << std::endl;
     
     // default stuff for setup
     OpenSSL_add_all_algorithms();
@@ -68,8 +109,15 @@ int main(int argc, char* argv[]) {
     // check what to run based on if enc/dec or salted
     if(encdec && salted) {
         encrypt(ctx, inFile, outFile, key, iv);
-    } else if (!encdec) {
-        // decrypt function would go here
+    } else if (encdec && !salted) {
+        encrypt(ctx, inFile, outFile, key, iv);
+    }
+
+    
+    if (!encdec && salted) {
+        decrypt(ctx, inFile, outFile, key, iv);
+    } else if (!encdec && !salted) {
+        decrypt(ctx, inFile, outFile, key, iv);
     }
 
     // close everything
